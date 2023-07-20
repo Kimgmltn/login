471c7f3c4c1c7cb97641423d0d95cfe384782dd2
khs <T1900843@partner.tmapmobility.com>
2023-07-20 17:14:07 +0900

    로그인 프로세스

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,14 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+
+}
